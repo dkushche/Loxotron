2a8a9b8986ebbd8d1b0ca7401f4ef4d202f8a077
Stanislav Kravchenko <krstanislav2018@gmail.com>
2023-02-19 18:20:16 +0200

    Basic registration

--- backend/index.js
+++ backend/index.js
@@ -22,3 +22,66 @@ app.get('/*', (req, res) => {
 })
 
 app.listen(PORT, () => console.log("SERVER STARTED ON PORT " + PORT))
+
+app.get('/login', (req, res) => {
+    res.render("login.ejs")
+
+})
+ 
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+  });
+  
+  // Создание модели пользователя
+  const User = mongoose.model('User', userSchema)
+  
+  
+  app.use(bodyParser.urlencoded({ extended: true }))
+  
+  
+  app.post('/users', function(req, res) {
+    // Обработка полученных данных из формы
+    const { username, password } = req.body
+  
+    // Создание нового пользователя
+    const newUser = new User({ username, password })
+    // Хэширование пароля перед сохранением в базу данных
+
+    userSchema.pre('save', function(next) {
+
+    const user = this
+
+    if (!user.isModified('password')) return next()
+
+    bcrypt.genSalt(10, function(err, salt) {
+      if (err) return next(err)
+
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) return next(err)
+
+        user.password = hash
+
+        next()
+      })
+    })
+  })
+  
+    // Сохранение пользователя в базе данных
+    newUser.save(function(err) {
+      if (err) {
+        // Отправка ошибки в ответе
+        res.status(500).send('Ошибка создания пользователя')
+      } else {
+        // Отправка сообщения об успешном создании пользователя в ответе
+        res.send('Пользователь создан успешно')
+      }
+    })
+  })
+
+
+
+
+
+    
+          
