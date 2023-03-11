bd9648068a0fb360b9e6bd7c4657c6f7e00c7edf
PeacefulAt0m <vlasiy.sv@gmail.com>
2023-03-11 12:26:08 +0200

    Set up webpack and create `LoxotronCasinoText` component

---
+++ frontend/src/components/LoxotronCasinoTextComponent/index.tsx
@@ -0,0 +1,9 @@
+import LoxotronCasinoText from "assets/svg/LoxotronCasinoText.svg"
+
+function LoxotronCasinoTextComponent(): JSX.Element {
+  return (
+      <LoxotronCasinoText/>
+  );
+}
+
+export default LoxotronCasinoTextComponent;
