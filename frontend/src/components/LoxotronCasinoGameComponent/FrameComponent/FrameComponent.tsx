1dae719c23056b3a8206898c0b31815e90796537
stas3729 <krstanislav2018@gmail.com>
2024-01-12 22:18:45 +0200

    Frame component

---
+++ frontend/src/components/LoxotronCasinoGameComponent/FrameComponent/FrameComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Frame from '../../../assets/svg/Frame.svg';
+
+const FrameComponent = () => {
+  return (
+    <div>
+      <Frame />
+    </div>
+  );
+};
+
+export default FrameComponent;
--- frontend/src/components/LoxotronCasinoGameComponent/GameComponent/GameComponent.tsx
+++ frontend/src/components/LoxotronCasinoGameComponent/GameComponent/GameComponent.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import PanelComponent from '../PanelComponent/PanelComponent';
 import SpinButton from '../../../assets/svg/SpinButton.svg';
 import './gameComponent.sass'
+import FrameComponent from '../FrameComponent/FrameComponent';
 
 const GameComponent: React.FC = () => {
   return (
-    <div className="game-component">
+    <div className="gameComponent">
       <PanelComponent />
       <div className='spinButton'>
         <SpinButton />
+      </div>
+      <div className='frame'>
+        <FrameComponent />
       </div>
     </div>
   );
