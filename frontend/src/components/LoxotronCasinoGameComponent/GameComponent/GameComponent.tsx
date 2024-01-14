cdbebeb54c8574ffa91ad752c6cf0a7ffc7f1257
stas3729 <krstanislav2018@gmail.com>
2024-01-14 21:49:45 +0200

    Add frame to game component

--- frontend/src/components/LoxotronCasinoGameComponent/GameComponent/GameComponent.tsx
+++ frontend/src/components/LoxotronCasinoGameComponent/GameComponent/GameComponent.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import PanelComponent from '../PanelComponent/PanelComponent';
 import SpinButton from '../../../assets/svg/SpinButton.svg';
-import './gameComponent.sass'
-import FrameComponent from '../FrameComponent/FrameComponent';
+import './gameComponent.sass';
+import Frame from '../../../assets/svg/Frame.svg';
 
 const GameComponent: React.FC = () => {
   return (
-    <div className="gameComponent">
+    <div className='gameComponent'>
       <PanelComponent />
       <div className='spinButton'>
         <SpinButton />
       </div>
       <div className='frame'>
-        <FrameComponent />
+        <Frame />
       </div>
     </div>
   );
