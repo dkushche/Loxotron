a6fc9e85aa0cddcbcdc6a4b3bf663414216ace0f
Stanislav Kravchenko <krstanislav2018@gmail.com>
2025-01-28 13:57:22 +0100

    Solve linter issues

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-import {User, UserDocument} from "src/auth/models/user.model";
+import { UserDocument} from "src/auth/models/user.model";
 import { Model } from "mongoose";
 import { JwtService } from "@nestjs/jwt";
 import { Response } from "express";
